98a5c7e8aeeab48b21cb716c412003a90c298182
Andreas Gohr <andi@splitbrain.org>
2014-02-16 17:08:30 +0100

    READMEs, more templates, and stuff

--- FileIconBuilder.php
+++ FileIconBuilder.php
@@ -1,28 +1,93 @@
 <?php
-
+/**
+ * Class FileIconBuilder
+ *
+ * Creates icons based on an file extension name and colors/templates associated with
+ * mime types
+ */
 class FileIconBuilder {
 
-    private $templatedir;
-    private $fontdir;
-    private $mimefile;
-    private $mimetypes;
-    private $colors;
+    protected $templatedir;
+    protected $fontdir;
+    protected $mimefile;
+    protected $mimetypes;
+    protected $colors;
 
-
-
-    public function __construct(){
+    /**
+     * Constructor
+     *
+     * Initializes the default colors
+     */
+    public function __construct() {
         $this->templatedir = __DIR__.'/templates/';
-        $this->fontdir = __DIR__.'/fonts/';
-        $this->mimefile = __DIR__.'/mime.types';
+        $this->fontdir     = __DIR__.'/fonts/';
+        $this->mimefile    = __DIR__.'/mime.types';
 
-        // fixme find good colors
+        // default colors, additions welcome
         $this->colors = array(
-            'image' => '#338833',
-            'text'  => '#888888',
-            'video' => '#883388',
-            'audio' => '#888833',
-            'application' => '#d22121'
+            // basic mime types
+            'image'       => '#000099',
+            'video'       => '#990099',
+            'audio'       => '#009900',
+            'text'        => '#999999',
+            'application' => '#990000',
+
+            // packages
+            'application-zip' => '#996600',
+            'application-x-gtar' => '#996600',
+            'application-x-bzip2' => '#996600',
+            'application-x-7z' => '#996600',
+
+            // office docs
+            'application-vnd-ms' =>  '#009999',
+            'application-msword' =>  '#009999',
+            'application-vnd-oasis-opendocument' =>  '#009999',
         );
+    }
+
+    /**
+     * Define a color for a mime type
+     *
+     * Colors can be given as HTML hex colors
+     *
+     * @param string $mime
+     * @param string $color
+     */
+    public function setColor($mime, $color) {
+        $this->colors[$mime] = $color;
+    }
+
+    /**
+     * Where to find the font files
+     *
+     * Needs trailing slash!
+     *
+     * @param string $fontdir
+     */
+    public function setFontdir($fontdir) {
+        $this->fontdir = $fontdir;
+    }
+
+    /**
+     * Where to find the known mimetypes
+     *
+     * This is a standard Linux /etc/mime.types files
+     *
+     * @param string $mimefile
+     */
+    public function setMimefile($mimefile) {
+        $this->mimefile = $mimefile;
+    }
+
+    /**
+     * Where to find the icon templates
+     *
+     * Needs trailing slash!
+     *
+     * @param string $templatedir
+     */
+    public function setTemplatedir($templatedir) {
+        $this->templatedir = $templatedir;
     }
 
     /**
@@ -32,13 +97,15 @@ class FileIconBuilder {
      */
     public function createAll($outdir) {
         if(!$this->mimetypes) $this->loadmimetypes();
-        mkdir("$outdir/16x16");
+        @mkdir($outdir);
+        @mkdir("$outdir/16x16");
+        @mkdir("$outdir/32x32");
 
         foreach(array_keys($this->mimetypes) as $ext) {
             $this->create16x16($ext, "$outdir/16x16/$ext.png");
+            $this->create32x32($ext, "$outdir/32x32/$ext.png");
         }
     }
-
 
     /**
      * Create a 16x16 icon for the given extension
@@ -47,59 +114,116 @@ class FileIconBuilder {
      * @param string $out output file (png)
      */
     public function create16x16($ext, $out) {
-        $im = imagecreatefrompng($this->templatedir.'16x16.png');
+        $box = array(0, 7, 15, 13);
+        $tpl = $this->ext2template($ext, '16x16');
+        $rgb = $this->ext2color($ext);
+
+        $im = imagecreatefrompng($tpl);
         imagesavealpha($im, true);
 
-        // dimensions of the extension bar
-        $left   = 0;
-        $right  = 14;
-        $top    = 7;
-        $bottom = 13;
-
-        list($r, $g, $b) = $this->ext2color($ext);
-
-        // draw the extension bar
-        for($x = $left; $x <= $right; $x++) {
-            for($y = $top; $y <= $bottom; $y++) {
-
-                // round the corners
-                $alpha = 0;
-                if($x == $left || $x == $right) {
-                    switch($y) {
-                        case $top:
-                        case $bottom:
-                            $alpha = 64;
-                            break;
-                        case $top + 1:
-                        case $bottom - 1:
-                            $alpha = 32;
-                            break;
-                    }
-                } elseif(($x == $left + 1 || $x == $right - 1) &&
-                    ($y == $top || $y == $bottom)
-                ) {
-                    $alpha = 32;
-                }
-
-                $c = imagecolorallocatealpha($im, $r, $g, $b , $alpha);
-                imagesetpixel($im, $x, $y, $c);
-            }
-        }
-
-        // calculate offset for centered text
-        $bbox  = imagettfbbox(6.0, 0, $this->fontdir.'pf_tempesta_five_compressed.ttf', strtoupper($ext));
-        $width  = $bbox[2];
-        $offset = floor(($right - ($left+1) - $width)/2.0);
-        if($offset < ($left+1)) $offset = ($left+1);
-
-        // write text
-        $c = imagecolorallocate($im, 255, 255, 255);
-        imagettftext($im, 6.0, 0, $offset, $bottom, -1 * $c,
-                     $this->fontdir.'pf_tempesta_five_compressed.ttf',
-                     strtoupper($ext));
+        $this->drawcolorbox($im, $box, $rgb, 'corner');
+        $this->drawtext($im, $box, strtoupper($ext), 6.0, 'pf_tempesta_five_compressed.ttf');
 
         imagepng($im, $out, 9);
         imagedestroy($im);
+    }
+
+    /**
+     * Create a 32x32 icon for the given extension
+     *
+     * @param string $ext extension name
+     * @param string $out output file (png)
+     */
+    public function create32x32($ext, $out) {
+        $box = array(4, 22, 26, 28);
+        $tpl = $this->ext2template($ext, '32x32');
+        $rgb = $this->ext2color($ext);
+
+        $im = imagecreatefrompng($tpl);
+        imagesavealpha($im, true);
+
+        $this->drawcolorbox($im, $box, $rgb, 'border');
+        $this->drawtext($im, $box, strtoupper($ext), 6.0, 'pf_tempesta_five.ttf');
+
+        imagepng($im, $out, 9);
+        imagedestroy($im);
+    }
+
+    /**
+     * Draws the extension text
+     *
+     * @param        $im
+     * @param array  $box
+     * @param string $text
+     * @param float  $size
+     * @param string $font
+     */
+    protected function drawtext($im, $box, $text, $size, $font) {
+        list($left, $top, $right, $bottom) = $box;
+
+        // calculate offset for centered text
+        $bbox   = imagettfbbox(6.0, 0, $this->fontdir.$font, $text);
+        $width  = $bbox[2];
+        $offset = floor(($right - $left  - $width) / 2.0);
+        $offset = $left + $offset;
+        //if($offset < $left) $offset = $left;
+
+        // write text
+        $c = imagecolorallocate($im, 255, 255, 255);
+        imagettftext(
+            $im, 6.0, 0, $offset, $bottom, -1 * $c,
+            $this->fontdir.$font,
+            $text
+        );
+    }
+
+    /**
+     * Draws the colored box for the extension text
+     *
+     * @param        $im
+     * @param array  $box
+     * @param array  $rgb
+     * @param string $bevel (border|corner)
+     */
+    protected function drawcolorbox($im, $box, $rgb, $bevel = '') {
+        list($left, $top, $right, $bottom) = $box;
+        list($r, $g, $b) = $rgb;
+
+        for($x = $left; $x <= $right; $x++) {
+            for($y = $top; $y <= $bottom; $y++) {
+
+                // Alpha transparency for bevels
+                $alpha = 0;
+                if($bevel == 'corner') {
+                    if($x == $left || $x == $right) {
+                        switch($y) {
+                            case $top:
+                            case $bottom:
+                                $alpha = 64;
+                                break;
+                            case $top + 1:
+                            case $bottom - 1:
+                                $alpha = 32;
+                                break;
+                        }
+                    } elseif(
+                        ($x == $left + 1 || $x == $right - 1) &&
+                        ($y == $top || $y == $bottom)
+                    ) {
+                        $alpha = 32;
+                    } elseif ( $y == $right ) {
+                        $alpha = 32;
+                    }
+                } else if($bevel == 'border') {
+                    if($x == $left || $x == $right) {
+                        $alpha = 32;
+                    }
+                }
+
+                $c = imagecolorallocatealpha($im, $r, $g, $b, $alpha);
+                imagesetpixel($im, $x, $y, $c);
+            }
+        }
     }
 
     /**
@@ -108,29 +232,57 @@ class FileIconBuilder {
      * @param string $ext
      * @return array RGB integers as array
      */
-    private function ext2color($ext) {
+    protected function ext2color($ext) {
         if(!$this->mimetypes) $this->loadmimetypes();
 
         // find the mimetype
-        if(isset($this->mimetypes[$ext])){
+        if(isset($this->mimetypes[$ext])) {
             $mime = $this->mimetypes[$ext];
         } else {
             $mime = 'application/octet-stream';
         }
+        $mime = preg_split('/[\/\.\-]/', $mime);
 
-        // find an associated color for exact match or class
-        if(isset($this->colors[$mime])) {
-            $color = $this->colors[$mime];
-        } else {
-            list($mime) = explode('/', $mime);
-            if (isset($this->colors[$mime])) {
-                $color = $this->colors[$mime];
-            } else {
-                $color = '#333333';
+        // try to find as exact match as possible
+        while(count($mime)) {
+            $test = join('-', $mime);
+            if(isset($this->colors[$test])) {
+                return $this->hex2rgb($this->colors[$test]);
             }
+            array_pop($mime);
         }
 
-        return $this->hex2rgb($color);
+        return $this->hex2rgb('#333333');
+    }
+
+    /**
+     * Returns a template for the given extension
+     *
+     * @param string $ext
+     * @param string $size
+     * @return string the template file
+     */
+    protected function ext2template($ext, $size) {
+        if(!$this->mimetypes) $this->loadmimetypes();
+
+        // find the mimetype
+        if(isset($this->mimetypes[$ext])) {
+            $mime = $this->mimetypes[$ext];
+        } else {
+            $mime = 'application/octet-stream';
+        }
+        $mime = preg_split('/[\/\.\-]/', $mime);
+
+        // try to find as exact match as possible
+        while(count($mime)) {
+            $test = join('-', $mime);
+            if(file_exists($this->templatedir."$size/$test.png")) {
+                return $this->templatedir."$size/$test.png";
+            }
+            array_pop($mime);
+        }
+
+        return $this->templatedir."$size/application.png";
     }
 
     /**
@@ -139,41 +291,41 @@ class FileIconBuilder {
      * @param string $hex HTML color code
      * @return array RGB integers as array
      */
-    private function hex2rgb($hex) {
+    protected function hex2rgb($hex) {
         // strip hash
         $hex = str_replace('#', '', $hex);
 
         // normalize short codes
-        if(strlen($hex) == 3){
-            $hex = substr($hex,0,1).
-                   substr($hex,0,1).
-                   substr($hex,1,1).
-                   substr($hex,1,1).
-                   substr($hex,2,1).
-                   substr($hex,2,1);
+        if(strlen($hex) == 3) {
+            $hex = substr($hex, 0, 1).
+                substr($hex, 0, 1).
+                substr($hex, 1, 1).
+                substr($hex, 1, 1).
+                substr($hex, 2, 1).
+                substr($hex, 2, 1);
         }
 
         // calc rgb
         return array(
-           hexdec(substr($hex, 0, 2)),
-           hexdec(substr($hex, 2, 2)),
-           hexdec(substr($hex, 4, 2))
+            hexdec(substr($hex, 0, 2)),
+            hexdec(substr($hex, 2, 2)),
+            hexdec(substr($hex, 4, 2))
         );
     }
 
     /**
      * Load mimetypes for extensions
      */
-    private function loadmimetypes() {
+    protected function loadmimetypes() {
         $this->mimetypes = array();
 
         $lines = file($this->mimefile);
-        foreach ($lines as $line){
+        foreach($lines as $line) {
             $exts = preg_split('/\s/', $line);
             $mime = array_shift($exts);
             if(!$exts) continue;
-            foreach($exts as $ext){
-                if(strlen($ext) > 3) continue; // we only handle 3 chars or less
+            foreach($exts as $ext) {
+                if(strlen($ext) > 4) continue; // we only handle 4 chars or less
                 $this->mimetypes[$ext] = $mime;
             }
         }
---
+++ README.md
@@ -0,0 +1,47 @@
+File Icon Generator
+===================
+
+This class can build pixel-perfect file type icons based on a extension name
+and its associated mime type.
+
+Icons are build based on a template file on which then a colored label with
+the extension is printed. This makes it easy to create a consistent look in
+your application without the problem of missing the icon for a certain file
+type.
+
+Currently 16x16 and 32x32 images can be generated.
+
+![Example Screenshot](example.png)
+
+Usage
+-----
+
+See createall.php for building all icons at once, otherwise just refer to the
+source code comments. It's relatively straight forward.
+
+MIME matching
+-------------
+
+For matching mime types in color and template setup the mime type of an extension
+is broken up parts at the charcters ```/```, ```.``` and ```-``` and connected
+with a ```-``` again.
+
+Partial matches are then tried per segment. Example:
+
+```application/vnd.openxmlformats-officedocument.wordprocessingml.document``` becomes
+```application-vnd-openxmlformats-officedocument-wordprocessingml-document```. It
+could be matched with ```application-vnd-openxmlformats``` or ```application```. The
+longest match wins. Note: ```wordprocessingml``` would not match!
+
+Todo
+----
+
+This can be improved, pull requests more than welcome!
+
+* define more colors
+* create more templates for common formats
+* add more output sizes
+* currently the same extension can be associated with multiple mime types
+  (eg. .doc) this makes it hard to know for which mime type to configure,
+  maybe provide a cleaned up mime.type file
+
